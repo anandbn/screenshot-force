0f6c0988eed07edfd07a5e9ab12a9b18f7e2496e
Anand Bashyam Narasimhan <anarasimhan@anarasimhan-ltm.internal.salesforce.com>
2018-07-03 12:12:10 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+### Prerequisites
+
+- NodeJS
+- NPM
+
+### Building 
+
+Download git repo and follow the below commands
+
+```
+npm install
+
+```
+
+### Running 
+
+Once build is complete, run the following command to get a screenshot of the wave dashboard
+
+```
+node index.js <username> <password> <dashboard labe>
+
+```
+
+__Note:__ This is currently only supporting production endpoint but the code is easy to follow for sandbox environments.
---
+++ index.js
@@ -0,0 +1,55 @@
+'use strict';
+const puppeteer = require('puppeteer');
+const Jimp = require('jimp');
+const opn = require('opn');
+const fs = require('fs');
+
+async function run() {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    const USERNAME_SELECTOR = '#username';
+    const PASSWORD_SELECTOR = '#password';
+    const BUTTON_SELECTOR = '#Login';
+    const APP_ICON_SELECTOR = '#oneHeader > div.bBottom > div > div.slds-context-bar__primary.navLeft > div.slds-context-bar__item.slds-context-bar_dropdown-trigger.slds-dropdown-trigger.slds-dropdown-trigger--click.slds-no-hover > nav > button';
+    const ALL_APPS_SELECTOR = "ul#sortable";
+    await page.goto('https://login.salesforce.com?startURL=/analytics/wave/wave.apexp#home');
+    await page.click(USERNAME_SELECTOR);
+    await page.keyboard.type(process.argv[2]);
+
+    await page.click(PASSWORD_SELECTOR);
+    await page.keyboard.type(process.argv[3]);
+
+    await page.click(BUTTON_SELECTOR);
+    console.log('Login button clicked');
+    await page.waitForNavigation();
+    console.log('after navigation complete');
+    await page.waitFor(6 * 1000);
+    console.log('after waiting for 5 seconds');
+    await page.screenshot({ path: 'screenshots/salesforce_after_login.png' , fullPage: true});
+
+    var dashboardName = process.argv[4];
+    console.log('Looking for Dashboard :'+dashboardName);
+    await page.click('[data-tooltip="'+dashboardName+'"]');
+    await page.waitForNavigation({waitUntil: 'networkidle2'});
+    console.log('after '+dashboardName);
+    await page.setViewport({width: 1200, height: 1200});
+    await page.waitFor(5 * 1000);
+
+    await page.screenshot({ path: 'screenshots/temp.png' , fullPage: true});
+    Jimp.read('screenshots/temp.png').then(function(theImage){
+        theImage.crop(0,150,1200,(1200-150))
+                .write("screenshots/"+dashboardName+".png");
+    });
+    await page.waitFor(2 * 1000);
+    fs.unlinkSync('screenshots/temp.png');
+    browser.close();
+    opn("screenshots/"+dashboardName+".png",{wait:false});
+}
+
+
+if(process.argv.length == 2){
+    console.error('Need to specify a Dashboard name');
+    process.exit(-1);
+}else{
+    run();
+}
